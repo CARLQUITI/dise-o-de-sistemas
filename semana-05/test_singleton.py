6b6c86b5d5cfb57726e443514aec3af97d6408fb
Carlos Quitigüiña <carlos.quitiguina003@gmail.com>
2026-09-15 14:01:26 -0500

    Semana-05-Actividad_En_Clase

---
+++ semana-05/test_singleton.py
@@ -0,0 +1,16 @@
+from singleton import GestorDeConfiguracion, reserva_permitida
+
+def test_rechaza_reserva():
+    config = GestorDeConfiguracion.obtener_objeto
+    config.modo_mantenimiento = False
+
+    assert reserva_permitida(config) is True
+
+def test_reserva_aceptada():
+    config = GestorDeConfiguracion.obtener_objeto
+
+    assert reserva_permitida(config) is True
+
+#Este falla porque el objeto está siendo tomado como una variable global dentro de test_singleton.py entonces al modificarse, una y otra vez cambia según la ultima
+#el objeto vaya pasando por las funciones y según se vaya modificando porque al final no esta creando un nuevo objeto o instancia sino reutilizando 
+# la misma de sigleton.py
