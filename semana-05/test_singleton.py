57de9be5d838a777ea29a268f75b8a9d483eead3
Carlos Quitigüiña <carlos.quitiguina003@gmail.com>
2026-09-15 14:03:01 -0500

    Semana-05_VersionFinal_Actividad_En_Clase

--- semana-05/test_singleton.py
+++ semana-05/test_singleton.py
@@ -11,6 +11,6 @@ def test_reserva_aceptada():
 
     assert reserva_permitida(config) is True
 
-#Este falla porque el objeto está siendo tomado como una variable global dentro de test_singleton.py entonces al modificarse, una y otra vez cambia según la ultima
+#Este falla porque el objeto está siendo tomado como una variable global dentro de test_singleton.py entonces al modificarse, una y otra vez cambia según
 #el objeto vaya pasando por las funciones y según se vaya modificando porque al final no esta creando un nuevo objeto o instancia sino reutilizando 
 # la misma de sigleton.py
